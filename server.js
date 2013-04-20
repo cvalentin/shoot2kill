41a048a03c88380efb7c7ea3796362af9bd1c251
Carlo Valentin <me@carlovalentin.com>
2013-04-19 19:22:55 -0700

    obj finish

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 //Objects
 
-function Player(x, y, id){
+
+function Player(x,y,name,avatar,id) { 
+	this.x = x;
+	this.y = y;
+	this.name = name;
+	this.avatar = avatar;
+	this.dir_x = 0;
+	this.dir_y = 0;
+	this.id = id;
+}
+
+function Bullet(x, y, id, p_id){
 	this.x = x;
 	this.y = y;
 	this.id = id;
-	this.
+	this.vel_x = 0;
+	this.vel_y = 0;
+	this.player_id = p_id;
 }
