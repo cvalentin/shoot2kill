f13b55f39de16c1321bf2901214828da02972a11
RafaelV <rafvertido@gmail.com>
2013-04-19 20:08:30 -0700

    rvertido
    Added creation of player id's

--- server.js
+++ server.js
@@ -19,10 +19,31 @@ function Bullet(x, y, id, p_id){
 	this.vel_y = 0;
 	this.player_id = p_id;
 
+//user_id to use for creation, starts at 0, incrememnts per player created
+var user_id = 0;
+
 //array of all players
-var _all_players = [];
+var all_players = [];
+
+//array of all bullets
+var all_bullets = [];
 
 //id for players, starts at 0, increment by 1 per player
-var _player_id_set = 0;
+var player_id_set = 0;
 
+// Start server -- Shiny code WOOO
+var stdin = process.openStdin();    
+var io = require('socket.io').listen(1500);
+io.set('log level', 1);
 
+io.sockets.on('connection', function(socket) {
+	//post a welcome message
+	io.sockets.emit('connect', {message:"connected!"});
+	
+	//give the player an id and add a new player object when an id is requested
+	socket.on('player_request_id', function(data) { 
+		data = eval(data)[0];
+		all_players.push(new Player(0,0,data.name,data.avatar,user_id);
+		user_id++;
+	});
+});
