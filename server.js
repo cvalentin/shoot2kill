8fa3e691e252a48596dd64855f9d2fca6465f574
Carlo Valentin <me@carlovalentin.com>
2013-04-19 21:19:56 -0700

    server update + bullet update

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ var _user_id = 0;
 var _all_players = [];
 
 //array of all bullets
-var all_bullets = [];
-var bullet_id = 0;
+var _all_bullets = [];
+var _bullet_id = 0;
 
 //id for players, starts at 0, increment by 1 per player
 var _player_id_set = 0;
@@ -41,6 +41,12 @@ var stdin = process.openStdin();
 var io = require('socket.io').listen(1500);
 io.set('log level', 1);
 
+//Server physics
+setInterval(function() {
+	game_update();
+
+},50);
+
 io.sockets.on('connection', function(socket) {
 	//post a welcome message
 	io.sockets.emit('connect', {message:"connected!"});
@@ -48,7 +54,7 @@ io.sockets.on('connection', function(socket) {
 	//give the player an id and add a new player object when an id is requested
 	socket.on('player_request_id', function(data) { 
 		data = eval(data)[0];
-		all_players.push(new Player(0,0,data.name,data.avatar,user_id);
+		all_players.push(new Player(0,0,data.name,data.avatar,user_id));
 		user_id++;
 	});
 
@@ -61,14 +67,28 @@ io.sockets.on('connection', function(socket) {
 			new Bullet(
 				all_players[data.player_id].x,
 				all_players[data.player_id].y,
-				bullet_id,
+				_bullet_id,
 				data.player_id,
 				vec.x(),
 				vec.y()
 			);
 		);
-		bullet_id++;
+		_bullet_id++;
 	});
 
-
+	var update_game = setInterval(function (){
+		io.sockets.emit('server_push', {players: _all_players, bullets: _all_bullets});
+	}, 50)
 });
+
+function game_update(){
+
+	//update bullet positions
+	for (var i=0;i<all_bullets.length;i++){
+		curr_bullet = all_bullets[i];
+		curr_bullet.x += curr_bullet.vel_x;
+		curr_bullet.y += curr_bullet.vel_y;
+		all_bullets[i] = curr_bullet;
+	}
+
+}
