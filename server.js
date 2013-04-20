8300a4b661ce7abb9ad3c87806acd91a62a07357
Carlo Valentin <me@carlovalentin.com>
2013-04-19 20:44:08 -0700

    server.js edit

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ function Player(x,y,name,avatar,id) {
 	this.avatar = avatar;
 	this.dir_x = 0;
 	this.dir_y = 0;
+	this.vel_x = 0;
+	this.vel_y = 0;
 	this.id = id;
 }
 
-function Bullet(x, y, id, p_id){
+function Bullet(x, y, id, p_id, vel_x, vel_y){
 	this.x = x;
 	this.y = y;
 	this.id = id;
-	this.vel_x = 0;
-	this.vel_y = 0;
+	this.vel_x = vel_x;
+	this.vel_y = vel_y;
 	this.player_id = p_id;
+}
 
 //user_id to use for creation, starts at 0, incrememnts per player created
 var user_id = 0;
@@ -27,6 +30,7 @@ var all_players = [];
 
 //array of all bullets
 var all_bullets = [];
+var bullet_id = 0;
 
 //id for players, starts at 0, increment by 1 per player
 var player_id_set = 0;
@@ -46,4 +50,19 @@ io.sockets.on('connection', function(socket) {
 		all_players.push(new Player(0,0,data.name,data.avatar,user_id);
 		user_id++;
 	});
+
+	//create bullet
+	socket.on('create_bullet', function(data) {
+		all_bullets.push(
+			new Bullet(
+				all_players[data.player_id].x,
+				all_players[data.player_id].y,
+				bullet_id,
+				data.player_id,
+				all_players[]
+			)
+		)
+	});
+
+
 });
