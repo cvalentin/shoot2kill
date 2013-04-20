f3668774adb066e897073238f6b21876e9c4062f
spotco <mootothemax@gmail.com>
2013-04-20 01:01:54 -0700

    fix

--- server/server.js
+++ server/server.js
@@ -6,59 +6,8 @@ var includeInThisContext = function(path) {
     vm.runInThisContext(code, path);
 }.bind(this);
 includeInThisContext("./chatserver.js");
-<<<<<<< HEAD
 includeInThisContext("../client/vector.js");
 includeInThisContext("./serverdef.js");
-=======
-
-function Pos(x,y) {
-	this.x = x;
-	this.y = y;
-}
-
-function Dir(dir_x,dir_y) {
-	this.x = dir_x;
-	this.y = dir_y;
-}
-function Vel(vel_x,vel_y) {
-	this.x = vel_x;
-	this.y = vel_y;
-}
-
-function Player(id,pos,dir,vel, name) { 
-	this.id = id;
-	this.pos = pos;
-	this.dir = dir;
-	this.vel = vel;
-	this.name = name;
-}
-
-function Bullet(id,player_id,pos,vel){
-	this.id = id;
-	this.player_id = player_id;
-	this.pos = pos;
-	this.vel = vel;
-}	
-
-function Walls(){
-}
-
-function cons_point(x,y) {
-	return {"x":x,"y":y};
-}
-
-function point_distance(a,b) {
-	return Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.y-b.y,2));
-}
-
-function rotate_by(pt,theta) {
-	var len = point_distance(pt,cons_point(0,0));
-	var curtheta = Math.atan2(pt.y,pt.x);
-	curtheta += theta;
-	var rtp = cons_point(Math.cos(curtheta),Math.sin(curtheta));
-	return rtp;
-}		
->>>>>>> 02be0267fa13221b428f17ef4b90b5d67f9095f5
 
 var _all_players = [];
 var _all_bullets = [];
